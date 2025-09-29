1ba1e3f12103ac1589efa8ae820499026280179c
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-09-29 18:13:28 -0300

    refactor: style header and add navigation

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
 
+  showHomeLink$: Observable<boolean>;
+
+  isWhiteBg$: Observable<boolean>;
+
+  private readonly pagesWithHomeLink = [
+    '/search',
+    '/hotel',
+    '/detail',
+    '/checkout',
+  ];
+
+  constructor(private router: Router) {
+    const navigation$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => event.urlAfterRedirects),
+      startWith(this.router.url)
+    );
+
+    this.showHomeLink$ = navigation$.pipe(
+      map((url) => this.pagesWithHomeLink.includes(url))
+    );
+
+    this.isWhiteBg$ = this.showHomeLink$;
+  }
+
+  pesquisar() {
+    this.router.navigate(['/']);
+  }
 }
